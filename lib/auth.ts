54ddcb6ac85cabc7722689b5d4a81bf83260d5af
shandilyaaryan <aryanshandilya10@gmail.com>
2025-03-01 22:38:38 +0530

    Added auth

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,47 @@
 import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        
-    ]
-}
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          throw new Error("Missing email and password.");
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+
+          if (!user) {
+            throw new Error("No user found!");
+          }
+
+          const isValidPass = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValidPass) {
+            throw new Error("Password Incorrect!");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          throw error;
+        }
+      },
+    }),
+  ],
+};
