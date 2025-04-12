95d6cb59c907f6b53eb5a8f4f83fcf7bb29a541f
shandilyaaryan <aryanshandilya10@gmail.com>
2025-04-12 12:07:13 +0000

    Added db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,29 +1,28 @@
 import mongoose from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL!
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-let cached = global.mongoose
-
-if (!cached){
-    cached = global.mongoose = { conn: null, promise: null}
+if (!MONGODB_URL) {
+    throw new Error("Please define mongodb URL in .env");
 }
 
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+};
+
 export const connectToDatabase = async () => {
-    if (cached.conn) {
+    if (cached.conn){
         return cached.conn;
     }
     if (!cached.promise){
         const opts = {
-            bufferCommands: true,
+            bufferedCommands: true,
             maxPoolSize: 10
-        }
+        };
 
-        cached.promise = mongoose.connect(MONGODB_URL, opts).then(() => mongoose.connection)
-    }
+        cached.promise = mongoose.connect(MONGODB_URL, opts).then(() => mongoose.connection);
 
-    try{
-        cached.conn = await cached.promise
-    } catch {
-        throw new Error("Error connecting to the database.")
     }
 }
--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,11 @@
-import { Connection } from "mongoose";
+import { Connection } from "mongoose"
 
-declare global {
+declare global{
   // eslint-disable-next-line no-var
   var mongoose: {
     conn: Connection | null;
     promise: Promise<Connection> | null;
-  };
+  }
 }
 
 export {};
