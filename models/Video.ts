640618d11075e80bf516fb080ebedc314e57ebe0
shandilyaaryan <aryanshandilya10@gmail.com>
2025-04-15 13:20:14 +0000

    Updated video.ts

--- models/Video.ts
+++ models/Video.ts
@@ -4,6 +4,7 @@ export const VIDEO_DIMENSIONS = {
   height: 1920,
   width: 1080,
 } as const;
+
 export interface IVideo {
   _id?: mongoose.Types.ObjectId;
   title: string;
@@ -11,4 +12,31 @@ export interface IVideo {
   videoUrl: string;
   thumbnailUrl: string;
   controls: boolean;
+  transformation?: {
+    height: number;
+    width: number;
+    quality?: number;
+  };
+  createdAt: Date;
+  updateAt: Date;
 }
+
+const videoSchema = new Schema<IVideo>({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  videoUrl: { type: String, required: true },
+  thumbnailUrl: { type: String, required: true },
+  controls: { type: Boolean, default: true },
+  transformation: {
+    height: { type: Number, default: VIDEO_DIMENSIONS.height },
+    width: { type: Number, default: VIDEO_DIMENSIONS.width },
+    quality: { type: Number, min: 1, max: 100 },
+  },
+  createdAt: { type: Date },
+  updateAt: { type: Date }
+}, { timestamps: true });
+
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
+
+export default Video;
